a57f9d420060fc916c37ceb2b7a8b26fe0a27a6e
johch3n611u <401450650@gapp.fju.edu.tw>
2021-02-19 12:53:35 +0800

    m

--- studio-backstage/src/app/app-routing.module.ts
+++ studio-backstage/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SignInComponent } from './components/account/sign-in/sign-in.component';
+import { SignInComponent } from './pages/account/sign-in/sign-in.component';
 import { DashboardComponent } from './components/layout/dashboard/dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AuthGuard } from './shared/auth/auth.guard';
