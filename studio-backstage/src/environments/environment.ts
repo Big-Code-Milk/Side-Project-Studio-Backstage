2b10b34852238e300c33c506272984e84cb0a987
johch3n611u <401450650@gapp.fju.edu.tw>
2021-02-21 14:09:26 +0800

    update .gitignore

--- studio-backstage/src/environments/environment.ts
+++ studio-backstage/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: "AIzaSyA0HTv4SIfnnbEptOuSnf8wmOGmgMJ0bgE",
     authDomain: "justdo-1165.firebaseapp.com",
