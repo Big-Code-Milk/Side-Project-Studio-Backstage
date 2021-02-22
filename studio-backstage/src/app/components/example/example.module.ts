17afb6d9547a50391d06bdc9791f1ac651fb5438
liu0912089608 <allen.liu@shinda.com.tw>
2021-02-22 17:02:48 +0800

    m

--- studio-backstage/src/app/components/example/example.module.ts
+++ studio-backstage/src/app/components/example/example.module.ts
@@ -4,15 +4,19 @@ import { CommonModule } from '@angular/common';
 import { ExampleRoutingModule } from './example-routing.module';
 import { BaseSharedModule } from '../../shared/module/base-shared.module';
 
+import { AngularFirebaseComponent } from './angular-fire-base/angular-firebase.component';
+
 @NgModule({
-  declarations: [],
+  declarations: [
+    AngularFirebaseComponent
+  ],
   imports: [
     CommonModule,
     ExampleRoutingModule,
     BaseSharedModule
   ],
   exports: [
-
+    AngularFirebaseComponent
   ]
 })
 export class ExampleModule { }
