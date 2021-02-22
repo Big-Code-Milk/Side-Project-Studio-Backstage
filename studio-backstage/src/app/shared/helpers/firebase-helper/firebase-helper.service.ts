cad2de12d66c95d93579adc2edae35190847abec
liu0912089608 <allen.liu@shinda.com.tw>
2021-02-22 17:14:43 +0800

    m

--- studio-backstage/src/app/shared/helpers/firebase-helper/firebase-helper.service.ts
+++ studio-backstage/src/app/shared/helpers/firebase-helper/firebase-helper.service.ts
@@ -1,24 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseHelperService {
-  // 實作類 Flux https://medium.com/4cats-io/%E6%B7%B1%E5%85%A5%E6%B7%BA%E5%87%BA-flux-44a48c320e11
-  // Interfase = Action
-  // Observable = Dispatcher
-  // Store = Service
-  // View = View
-
-  // 方法可參考 angularfire 套件官網 https://github.com/angular/angularfire
+  // 參考
+  // 實作類似 Flux https://medium.com/4cats-io/%E6%B7%B1%E5%85%A5%E6%B7%BA%E5%87%BA-flux-44a48c320e11
+  // angularfire 套件官網 https://github.com/angular/angularfire
+  // [Angular Firebase 入門與實做] https://ithelp.ithome.com.tw/m/articles/10193310
+  // [Angular] 與 Firebase 共舞 https://blog.kevinyang.net/2018/04/30/angular-firebase/
+  // 免費額度 https://firebase.google.com/pricing/
 
   // 建構子( 注入此類別參數 ){ 建構子執行之方法 }
   constructor(
-    private _RealtimeDatabase: AngularFireDatabase
+    private _RealtimeDatabase: AngularFireDatabase,
+    private _CloudFirestore: AngularFirestore
   ) { }
-  // 免費額度 https://firebase.google.com/pricing/
 
   // 方法名稱< 型別 >( 參數:型別 ) : 回傳值型別 { 方法實作 return 回傳值; }
   GetObject<T>(url: string): Observable<T> | Observable<any> {
@@ -30,8 +30,6 @@ export class FirebaseHelperService {
     const _data = this._RealtimeDatabase.list(url);
     return _data.valueChanges();
   }
-
-  // 詳細可參考 https://ithelp.ithome.com.tw/m/articles/10193310
 
   GetObjectByKey<T>(url: string): Observable<T> | Observable<any> {
     const _data = this._RealtimeDatabase.object(url);
