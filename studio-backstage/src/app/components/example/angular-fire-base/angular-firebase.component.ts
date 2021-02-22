9653e9c51f3012a33e68de54c0d8a8ce695f743c
johch3n611u <401450650@gapp.fju.edu.tw>
2021-02-22 22:33:42 +0800

    M

--- studio-backstage/src/app/components/example/angular-fire-base/angular-firebase.component.ts
+++ studio-backstage/src/app/components/example/angular-fire-base/angular-firebase.component.ts
@@ -12,7 +12,7 @@ export class AngularFirebaseComponent implements OnInit {
 
   items$: Observable<any[]>;
   item$: Observable<any>;
-  ServiceItems: Observable<any[]>;
+  //ServiceItems: Observable<any[]>;
 
   constructor(
     private _FireStorageHelper: FireStorageHelperService,
@@ -20,13 +20,14 @@ export class AngularFirebaseComponent implements OnInit {
   ) {
     console.log('this._db', this._db);
     // 訂閱觀察者物件後，雲 db 資料改動，同時也會影響此處資料改動
-    this.items$ = this._db.list("item").valueChanges();
+    // snapshotChanges() 資料本身(payload)、key、prevKey、type
+    this.items$ = this._db.list("item").snapshotChanges();
 
-    // 直接使用 fireAG 方法操作
+    // 直接使用 fireAG 方法操作 valueChanges() 資料本身不包含key
     this.item$ = this._db.object('item').valueChanges();
 
     // 利用共用 Service 取得觀察者物件
-    this.ServiceItems = this._FireStorageHelper.GetSnapshotChangesList("item");
+    //this.ServiceItems = this._FireStorageHelper.GetObservableList("item");
 
     // 觀察者物件在訂閱回傳時 console.log
     this.item$.subscribe(x => { console.log('subscribe:' + x); });
--- studio-backstage/src/app/shared/helpers/fire-storage-helper/fire-storage-helper.service.ts
+++ studio-backstage/src/app/shared/helpers/fire-storage-helper/fire-storage-helper.service.ts
@@ -1,18 +1,19 @@
+// 參考
+// 實作類似 Flux https://medium.com/4cats-io/%E6%B7%B1%E5%85%A5%E6%B7%BA%E5%87%BA-flux-44a48c320e11
+// angularfire 套件官網 https://github.com/angular/angularfire
+// [Angular Firebase 入門與實做] https://ithelp.ithome.com.tw/m/articles/10193310
+// [Angular] 與 Firebase 共舞 https://blog.kevinyang.net/2018/04/30/angular-firebase/
+// 免費額度 https://firebase.google.com/pricing/
+
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';
 import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FireStorageHelperService {
-  // 參考
-  // 實作類似 Flux https://medium.com/4cats-io/%E6%B7%B1%E5%85%A5%E6%B7%BA%E5%87%BA-flux-44a48c320e11
-  // angularfire 套件官網 https://github.com/angular/angularfire
-  // [Angular Firebase 入門與實做] https://ithelp.ithome.com.tw/m/articles/10193310
-  // [Angular] 與 Firebase 共舞 https://blog.kevinyang.net/2018/04/30/angular-firebase/
-  // 免費額度 https://firebase.google.com/pricing/
 
   // 建構子( 注入此類別參數 ){ 建構子執行之方法 }
   constructor(
@@ -24,23 +25,18 @@ export class FireStorageHelperService {
   }
 
   // 方法名稱< 型別 >( 參數:型別 ) : 回傳值型別 { 方法實作 return 回傳值; }
-  GetValueChangesObject<T>(ValuePath: string): Observable<T> | Observable<any> {
-    const _data = this._RealtimeDatabase.object(ValuePath);
-    return _data.valueChanges(); // 資料本身不包含key
+  // GetObservableList<T>(ValuePath: string): Observable<T> | Observable<any> {
+  //   const _data = this._RealtimeDatabase.list(ValuePath);
+  //   return _data.valueChanges();
+  // }
+
+  // T 泛型 傳啥進來就啥型別
+
+  GetAngularFireObject<T>(ValuePath: string): AngularFireObject<T> {
+    return this._RealtimeDatabase.object(ValuePath);
   }
 
-  GetValueChangesList<T>(ValuePath: string): Observable<T> | Observable<any> {
-    const _data = this._RealtimeDatabase.list(ValuePath);
-    return _data.valueChanges();
-  }
-
-  GetSnapshotChangesObject<T>(ValuePath: string): Observable<T> | Observable<any> {
-    const _data = this._RealtimeDatabase.object(ValuePath);
-    return _data.snapshotChanges(); // 資料本身(payload)、key、prevKey、type
-  }
-
-  GetSnapshotChangesList<T>(ValuePath: string): Observable<T> | Observable<any> {
-    const _data = this._RealtimeDatabase.list(ValuePath);
-    return _data.snapshotChanges();
+  GetAngularFireList<T>(ValuePath: string): AngularFireList<T> {
+    return this._RealtimeDatabase.list(ValuePath);
   }
 }
