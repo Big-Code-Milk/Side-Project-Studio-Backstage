7ff67f30d6854d8ec74fdbbf275fb343870a2e15
liu0912089608 <allen.liu@shinda.com.tw>
2021-02-23 15:10:14 +0800

    m

--- studio-backstage/src/environments/environment.prod.ts
+++ studio-backstage/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA0HTv4SIfnnbEptOuSnf8wmOGmgMJ0bgE",
+    authDomain: "justdo-1165.firebaseapp.com",
+    databaseURL: "https://justdo-1165-default-rtdb.firebaseio.com",
+    projectId: "justdo-1165",
+    storageBucket: "justdo-1165.appspot.com",
+    messagingSenderId: "865876645704",
+    appId: "1:865876645704:web:81d74f5362557c9cfcc57d",
+    measurementId: "G-XS9L7WPC9W"
+  }
 };
