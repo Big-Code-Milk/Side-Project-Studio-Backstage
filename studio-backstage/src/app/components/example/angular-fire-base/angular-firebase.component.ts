8f8a18e980e5f545d9c120bcb2b50f34dacc19e6
johch3n611u <401450650@gapp.fju.edu.tw>
2021-03-07 00:10:56 +0800

    m

--- studio-backstage/src/app/components/example/angular-fire-base/angular-firebase.component.ts
+++ studio-backstage/src/app/components/example/angular-fire-base/angular-firebase.component.ts
@@ -69,7 +69,7 @@ export class AngularFirebaseComponent implements OnInit {
 
 
     // collection 要建立索引才能多重搜索... 建立等半天就先睡了
-    var collection = this._AngularFirestore.collection('Task', ref => ref.where('Tags', '!=', '未處理').orderBy('Tags').orderBy('EndDate'));
+    var collection = this._AngularFirestore.collection('Task', ref => ref.where('Tags', 'array-contains-any', ['未處理']).orderBy('EndDate'));
     collection.valueChanges().subscribe(x => { console.log('collection', x); })
 
   }
