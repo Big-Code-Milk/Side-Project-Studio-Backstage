4ada3d8cc9205be6b2e6aea65267f912ed3692de
johch3n611u <401450650@gapp.fju.edu.tw>
2021-03-18 13:13:26 +0800

    m

--- studio-backstage/src/app/components/layout/editor/editor-md/editor-md.component.ts
+++ studio-backstage/src/app/components/layout/editor/editor-md/editor-md.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { EditorConfig } from '../../../../shared/directive/editor-md/editor-md-config';
 
 declare var editormd: any;
@@ -11,13 +11,24 @@ declare var $: any;
 })
 export class EditorMdComponent implements OnInit {
 
+  @Output() onEditorMdChange: EventEmitter<string> = new EventEmitter<string>(); // 发射器
   _EditorConfig = new EditorConfig;
   _EditorMd: any;
+  Markdown: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    // 初始化
     this._EditorMd = editormd('EditorMd', this._EditorConfig);
+    // 当编辑器内容改变时，触发textarea的change事件
+    let ChangeEvent = this.onEditorMdChange;
+    let ChangeValue = this.Markdown;
+    this._EditorMd.on('change', function () {
+      console.log('debug', ChangeValue);
+      ChangeEvent.emit(ChangeValue);
+    });
   }
+
 
 }
