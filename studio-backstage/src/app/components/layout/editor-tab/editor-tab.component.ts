04a90019a9c58a5c003f3fd98a4952b54f6f0d60
liu0912089608 <allen.liu@shinda.com.tw>
2021-03-19 16:33:06 +0800

    M

--- studio-backstage/src/app/components/layout/editor-tab/editor-tab.component.ts
+++ studio-backstage/src/app/components/layout/editor-tab/editor-tab.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CKEditorComponent } from 'ng2-ckeditor';
 
 @Component({
   selector: 'app-editor-tab',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorTabComponent implements OnInit {
 
+  EditorMode: string = "";
+  Content: string = "";
+
   constructor() { }
 
   ngOnInit(): void {
+    this.InitCkeditor();
   }
 
+  EditorModeChange() {
+    console.log('EditorMode', this.EditorMode);
+  }
+
+  // ng2-ckeditor
+  name = 'ng2-ckeditor';
+  ckeConfig: CKEDITOR.config;
+  log: string = '';
+  @ViewChild("myckeditor") ckeditor: CKEditorComponent;
+
+  InitCkeditor() {
+    this.ckeConfig = {
+      allowedContent: false,
+      extraPlugins: 'divarea',
+      forcePasteAsPlainText: true,
+      height: '45vh',
+    },
+      this.Content = `<p>My html content</p>`;
+  }
+
+  onChange($event: any): void {
+    console.log("onChange"),
+      //this.log += new Date() + "<br />" ,
+      console.log('this.mycontent', this.Content)
+  }
+
+  onPaste($event: any): void {
+    console.log("onPaste");
+    //this.log += new Date() + "<br />" ;
+  }
+
+  // editor.md
+  SyncModel(Value: any): void {
+    console.log('Value', Value);
+    this.Content = Value;
+  }
 }
