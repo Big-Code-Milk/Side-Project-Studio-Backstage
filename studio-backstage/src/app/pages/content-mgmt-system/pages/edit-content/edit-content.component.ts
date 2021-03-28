17dca2cf268c78ac35ec1b96d0305ae087fbf956
johch3n611u <401450650@gapp.fju.edu.tw>
2021-03-28 18:22:43 +0800

    m

--- studio-backstage/src/app/pages/content-mgmt-system/pages/edit-content/edit-content.component.ts
+++ studio-backstage/src/app/pages/content-mgmt-system/pages/edit-content/edit-content.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import GtdTask from 'src/app/shared/models/gtd-task';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { MatAutocompleteSelectedEvent, MatAutocomplete } from '@angular/material/autocomplete';
+import { ViewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-edit-content',
@@ -7,10 +16,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditContentComponent implements OnInit {
 
+  DisplayMode1: boolean;
+  DisplayMode2: boolean;
   HTMLContent: string;
   MarkdownContent: string;
+  GtdTask: GtdTask = new GtdTask();
+  Term: FormGroup;
+  Tags: string[] = [];
+  _FormControl = new FormControl();
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  _FilteredTags: Observable<string[]>;
+  allTags: string[] = ['工作室', '行銷', '架構', '技術', '業務'];
 
-  constructor() { }
+  constructor() {
+    this._FilteredTags = this._FormControl.valueChanges.pipe(
+      startWith(null),
+      map((Tag: string | null) => Tag ? this._filter(Tag) : this.allTags.slice()));
+  }
 
   ngOnInit(): void {
   }
@@ -19,5 +41,42 @@ export class EditContentComponent implements OnInit {
     console.log('最外層 Value', Value);
     this.HTMLContent = Value.HTMLContent;
     this.MarkdownContent = Value.MarkdownContent;
+  }
+
+  remove(Tag: string): void {
+    const index = this.Tags.indexOf(Tag);
+
+    if (index >= 0) {
+      this.Tags.splice(index, 1);
+    }
+  }
+
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our fruit
+    if ((value || '').trim()) {
+      this.Tags.push(value.trim());
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+
+    this._FormControl.setValue(null);
+  }
+
+  @ViewChild('TagInput') TagInput: ElementRef<HTMLInputElement>;
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.Tags.push(event.option.viewValue);
+    this.TagInput.nativeElement.value = '';
+    this._FormControl.setValue(null);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.allTags.filter(Tag => Tag.toLowerCase().indexOf(filterValue) === 0);
   }
 }
