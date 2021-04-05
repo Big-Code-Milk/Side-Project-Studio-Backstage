5f821d6647756f39da8dfd579c4500efa9433869
johch3n611u <401450650@gapp.fju.edu.tw>
2021-04-05 23:50:55 +0800

    m

--- studio-backstage/src/app/pages/backstage-pages/template/template.module.ts
+++ studio-backstage/src/app/pages/backstage-pages/template/template.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { TemplateRoutingModule } from './template-routing.module';
 import { HomeComponent } from './home/home.component';
 import { SharedAngularMaterialModule } from '../../../shared/module/angular-material.module';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, CardComponent],
   imports: [
     CommonModule,
     TemplateRoutingModule,
