5a3853612d05b7d61a62ea0bcf71b78eda355d2e
johch3n611u <401450650@gapp.fju.edu.tw>
2021-04-21 00:31:21 +0800

    m

--- studio-backstage/src/app/pages/backstage-pages/components/layout/imgur/imgur.component.ts
+++ studio-backstage/src/app/pages/backstage-pages/components/layout/imgur/imgur.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-imgur',
@@ -9,8 +10,12 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 })
 export class ImgurComponent implements OnInit {
 
+  // https://ittone.ma/ittone/javascript-unable-to-upload-to-imgur-when-using-localhost/
+  // 超級無敵大坑，乎Imgur可能會限制直接從本地主機訪問映像，相同程式碼上 github page 就成功了
+
   constructor(
     protected _HttpClient: HttpClient, // https://stackoverflow.com/questions/47236963/no-provider-for-httpclient
+    protected _DomSanitizer: DomSanitizer,
   ) { }
 
   ngOnInit(): void {
@@ -20,20 +25,22 @@ export class ImgurComponent implements OnInit {
 
   ImgFileChange(Event: any) {
     // console.log(Event);
-    this.ImgObject.ImgFile = Event.target.files[0]; // input type="file" 的值
-    this.ImgObject.ImgFile.name; // input的圖檔名稱
-    this.ImgObject.ImgFileSize = Math.floor(this.ImgObject.ImgFile.size * 0.001) + 'KB'; // input的圖片大小
-    this.ImgObject.ImgFileThumbnail = window.URL.createObjectURL(this.ImgObject.ImgFile); // input的圖片縮圖
-    // console.log(this.ImgObject);
+    if (Event.target.files.length > 0) {
+      this.ImgObject.ImgFile = Event.target.files[0]; // input type="file" 的值
+      this.ImgObject.ImgFile.name; // input的圖檔名稱
+      this.ImgObject.ImgFileSize = Math.floor(this.ImgObject.ImgFile.size * 0.001) + 'KB'; // input的圖片大小
+      this.ImgObject.ImgFileThumbnail = this._DomSanitizer.bypassSecurityTrustUrl(window.URL.createObjectURL(this.ImgObject.ImgFile)); // input的圖片縮圖
+      // console.log(this.ImgObject);
+    }
   }
 
   async UploadImgur() {
 
     if (this.ImgObject.ImgFile != undefined) {
 
-      const Url = "https://api.imgur.com/3/image?withCredentials=false";
+      const Url = "https://api.imgur.com/3/image";
       let _FormData = new FormData();
-      _FormData.append('image', this.ImgObject.ImgFile);
+      _FormData.append('image', this.ImgObject.ImgFile, this.ImgObject.ImgFile.name);
 
       const httpOptions = {
         headers: new HttpHeaders({
