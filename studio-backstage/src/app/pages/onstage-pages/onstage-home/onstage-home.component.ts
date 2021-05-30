bf7e2b98352b8e4a7c13930ca2025ec2755474cc
johch3n611u <401450650@gapp.fju.edu.tw>
2021-05-30 22:35:06 +0800

    gsap

--- studio-backstage/src/app/pages/onstage-pages/onstage-home/onstage-home.component.ts
+++ studio-backstage/src/app/pages/onstage-pages/onstage-home/onstage-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { TweenMax, gsap } from "gsap";
 
 @Component({
   selector: 'app-onstage-home',
@@ -12,10 +13,15 @@ export class OnstageHomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  @ViewChild("Desktop") private _Desktop: ElementRef;
+  @ViewChild("Img1") private _Img1: ElementRef;
+  @ViewChild("Img2") private _Img2: ElementRef;
+  @ViewChild("Img3") private _Img3: ElementRef;
+  @ViewChild("Img4") private _Img4: ElementRef;
 
 
   ngAfterViewInit(): void {
-    console.log(this._Desktop);
+    console.log(this._Img1);
+    console.log('gsap', gsap);
+    gsap.to(this._Img1.nativeElement, { duration: 2, x: 300 });
   }
 }
