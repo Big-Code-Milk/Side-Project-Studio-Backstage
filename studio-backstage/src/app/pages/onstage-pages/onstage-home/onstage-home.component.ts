c2feaeded32e6ff96970d1c11a659a1492a07dc1
johch3n611u <401450650@gapp.fju.edu.tw>
2021-06-11 13:39:55 +0800

    m

--- studio-backstage/src/app/pages/onstage-pages/onstage-home/onstage-home.component.ts
+++ studio-backstage/src/app/pages/onstage-pages/onstage-home/onstage-home.component.ts
@@ -29,6 +29,7 @@ export class OnstageHomeComponent implements OnInit {
     console.log('gsap', gsap);
     this.Part1Animation();
     this.Part2Animation();
+    this.Part3Animation();
   }
 
   @ViewChild("Img1") private _Img1: ElementRef;
@@ -81,5 +82,42 @@ export class OnstageHomeComponent implements OnInit {
     tl6.to(this._Img9.nativeElement, { duration: 2, opacity: 0.2, delay: 7 });
     tl7.to(this._Img10.nativeElement, { duration: 2, opacity: 0.2, delay: 7 });
     tl.to(this._Img11.nativeElement, { duration: 2, x: 81, y: 131, opacity: 1, scale: 1, ease: "bounce", delay: 2 });
+  }
+
+  @ViewChild("Img12") private _Img12: ElementRef;
+  @ViewChild("Img13") private _Img13: ElementRef;
+  @ViewChild("Img14") private _Img14: ElementRef;
+  @ViewChild("Img15") private _Img15: ElementRef;
+  @ViewChild("Img16") private _Img16: ElementRef;
+  @ViewChild("Img17") private _Img17: ElementRef;
+  @ViewChild("Img18") private _Img18: ElementRef;
+  @ViewChild("Img19") private _Img19: ElementRef;
+
+  Part3Animation() {
+    var tl = gsap.timeline();
+    var tl8 = gsap.timeline();
+    var tl2 = gsap.timeline();
+    var tl3 = gsap.timeline();
+    var tl4 = gsap.timeline();
+    var tl5 = gsap.timeline();
+    var tl6 = gsap.timeline();
+    var tl7 = gsap.timeline();
+    var tl9 = gsap.timeline();
+    var tl10 = gsap.timeline();
+
+    tl.to(this._Img12.nativeElement, { duration: 2, x: -170, y: -20, opacity: 1, ease: "bounce" });
+    tl.to(this._Img13.nativeElement, { duration: 2, x: -204, y: -34, opacity: 1, ease: "bounce" });
+    tl.to(this._Img14.nativeElement, { duration: 2, x: -164, y: -21, opacity: 1, ease: "bounce" });
+    tl8.to(this._Img15.nativeElement, { duration: 2, x: -107, y: -3, opacity: 1, ease: "bounce" });
+    tl8.to(this._Img16.nativeElement, { duration: 2, x: -209, y: -3, opacity: 1, ease: "bounce" });
+    tl8.to(this._Img17.nativeElement, { duration: 2, x: -216, y: 33, opacity: 1, ease: "bounce" });
+    tl2.to(this._Img12.nativeElement, { duration: 2, opacity: 0.2, delay: 7 });
+    tl3.to(this._Img13.nativeElement, { duration: 2, opacity: 0.2, delay: 7 });
+    tl4.to(this._Img14.nativeElement, { duration: 2, opacity: 0.2, delay: 7 });
+    tl5.to(this._Img15.nativeElement, { duration: 2, opacity: 0.2, delay: 7 });
+    tl6.to(this._Img16.nativeElement, { duration: 2, opacity: 0.2, delay: 7 });
+    tl7.to(this._Img17.nativeElement, { duration: 2, opacity: 0.2, delay: 7 });
+    tl9.to(this._Img18.nativeElement, { rotate: -15, duration: 2, x: 20, y: 189, opacity: 1, scale: 1, ease: "bounce", delay: 9 });
+    tl10.to(this._Img19.nativeElement, { rotate: 10, duration: 2, x: 133, y: 93, opacity: 1, scale: 1, ease: "bounce", delay: 9 });
   }
 }
